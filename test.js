5690c9eb4a0fa666980f78d5cc3c7d5c14cd8f7b
Hussnain Fareed <hasni.rock@yahoo.com>
2022-09-20 22:02:28 +0500

    Nonce string method fixed and Hash generated

--- test.js
+++ test.js
@@ -5,4 +5,21 @@ const bitcoin = new Blockchain();
 bitcoin.createNewBlock(1234, "kjhdalsdkfgkjdsfslkjdfh", "oihfiuagfkjdsfkj");
 bitcoin.createNewBlock(2658, "oihfiuagfkjdsfkj", "oihfiuagfkjdsfkjtwo");
 
-console.log(bitcoin);
+const previousBlockHash = "fgksdjfhiuweury8934r2i3rkaejfbiaw88urekrfj";
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: "kjdshflisdhfiu093u4kd",
+        receiver: "klfdkhfdskfdshudfsk"
+    },
+    {
+        amount: 20,
+        sender: "kasdjdshflisdhfiu093u4kd",
+        receiver: "klkyukjhfdkhfdskfdshudfsk"
+    }
+]
+const nonce = 5485616;
+
+var hash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+console.log(hash);
