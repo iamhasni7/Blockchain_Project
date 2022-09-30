af667f994173dccd89ccfd5b811060e9e4fb2582
Hussnain Fareed <hasni.rock@yahoo.com>
2022-09-30 11:55:37 +0500

    GET and POST methods added

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+const bodyParser = require("body-parser");
 
-app.get("/", function (req, res) {
-    res.send("Blockchain is the future!")
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get("/blockchain", function (req, res) {
+    res.send(bitcoin);
+})
+
+app.post("/transaction", function (req, res) {
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({note: 'This transaction will be added in block ${blockIndex}'});
 })
 
 app.listen(3000);
