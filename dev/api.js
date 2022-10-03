373f7bf04ba920d22ca232e1074132411b71d84f
Hussnain Fareed <hasni.rock@yahoo.com>
2022-10-03 20:33:31 +0500

    New End-Point added (mineNewBlock)

--- dev/api.js
+++ dev/api.js
@@ -3,17 +3,42 @@ const app = express();
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
 const bodyParser = require("body-parser");
+const {v4: uuid} = require("uuid");
+
+const nodeAddress = uuid().split("-").join("");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+// This End-Point will provide us complete blockchain.
 app.get("/blockchain", function (req, res) {
     res.send(bitcoin);
 })
 
+//This End-Point will add a new transaction.
 app.post("/transaction", function (req, res) {
     const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
-    res.json({note: 'This transaction will be added in block ${blockIndex}'});
+    res.json({note: `This transaction will be added in block ${blockIndex}`});
+})
+
+// This End-Point will add a new block into the blockchain.
+app.get("/mineNewBlock", function (req, res) {
+    const lastBlock = bitcoin.getLastBlock();
+    const prevBlockHash = lastBlock["hash"];
+
+    const currentBlockData = {
+        transaction: bitcoin.pendingTransactions,
+        index: lastBlock["index"] + 1
+    }
+
+    bitcoin.createNewTransaction(10, "0000000000", nodeAddress);
+
+    const nonce = bitcoin.proofOfWork(prevBlockHash, currentBlockData);
+
+    const blockHash = bitcoin.hashBlock(prevBlockHash, currentBlockData, nonce);
+
+    const newBlock = bitcoin.createNewBlock(nonce, prevBlockHash, blockHash);
+    res.json({note: "New block mined successfully", block: newBlock});
 })
 
 app.listen(3000);
