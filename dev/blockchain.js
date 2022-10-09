b60a23f5ac4077fce4c7f3c2e390f421c9c3e41b
Muhammad Hussnain Fareed <hasni.rock@yahoo.com>
2022-10-09 15:20:04 +0500

    Minor bugs fixed

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,10 +1,11 @@
 const sha256 = require('sha256');
+const currentNodeUrl = process.argv[3];
 
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
-
     this.createNewBlock(100, "0", "0"); //Genesis block created.
+    this.currentNodeUrl = currentNodeUrl;
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, prevBlockHash, hash) {
